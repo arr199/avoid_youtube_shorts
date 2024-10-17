196bb0c750d5f261e6e64fc6cfb0230b8f7f1cda
arr199 <abieledelso@gmail.com>
2024-10-17 21:55:22 +0100

    bloc other social media platform

--- content.js
+++ content.js
@@ -4,7 +4,25 @@ const CONSTANTS = {
 
 const URLS = {
   YOUTUBE_SHORTS: "https://www.youtube.com/shorts/",
+  TIKTOK: "https://www.tiktok.com/",
+  INSTAGRAM: "https://www.instagram.com/",
+  X: "https://x.com/",
 };
+
+const notification = document.createElement("div");
+notification.style.position = "fixed";
+notification.style.top = "0";
+notification.style.left = "0";
+notification.style.width = "100%";
+notification.style.height = "50px";
+notification.style.backgroundColor = "red";
+notification.style.color = "white";
+notification.style.display = "flex";
+notification.style.justifyContent = "center";
+notification.style.alignItems = "center";
+notification.style.zIndex = "9999";
+notification.innerHTML =
+  "Get the fuck out of this page and start doing something productive";
 
 function avoidShorts() {
   const url = window.location.href;
@@ -12,6 +30,22 @@ function avoidShorts() {
   if (url.startsWith(URLS.YOUTUBE_SHORTS)) {
     location.href = "https://www.youtube.com/";
     // window.open("https://www.google.com", "_self");
+  }
+
+  if (url.startsWith(URLS.TIKTOK)) {
+    console.log("CLOSING WINDOW");
+    document.body.innerHTML = "";
+    document.body.appendChild(notification);
+  }
+  if (url.startsWith(URLS.INSTAGRAM)) {
+    console.log("CLOSING WINDOW");
+    document.body.innerHTML = "";
+    document.body.appendChild(notification);
+  }
+  if (url.startsWith(URLS.X)) {
+    console.log("CLOSING WINDOW");
+    document.body.innerHTML = "";
+    document.body.appendChild(notification);
   }
 }
 
