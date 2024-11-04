c9e0032bd9fc7929b7411ec09df52c9042a11c5d
arr199 <abieledelso@gmail.com>
2024-11-04 12:52:05 +0000

    add new logic to save and get data from chrome sync storage

--- index.js
+++ index.js
@@ -1,84 +1,101 @@
 // TODO !IMPORTANT : Fix the bug where the checkbox is not checked when the page is reloaded
 // TODO : Add a feature to remove a site from the list
 
+//  CONSTANTS
 const SELECTORS = {
   FORM: "form",
   SITE_LIST_CONTAINER: "#site-list-container",
-  YOUTUBE: "#youtube",
   ADD_SITE_INPUT: "#add-site-input",
   ADD_SITE_BUTTON: "#add-site-btn",
 };
+const SITES_LIST = "sitesList";
+
+// GET HTML ELEMENTS HELPERS
 const get = (selector) => document.querySelector(selector);
 const getAll = (selector) => document.querySelectorAll(selector);
 
 const formEl = get(SELECTORS.FORM);
-/**
- * @type {HTMLDivElement}
- */
-
 const siteListContainerEl = get(SELECTORS.SITE_LIST_CONTAINER);
 const addSiteInputEl = get(SELECTORS.ADD_SITE_INPUT);
 const addSiteButtonEl = get(SELECTORS.ADD_SITE_BUTTON);
 
-createSiteList();
-const inputs = getAll(`${SELECTORS.SITE_LIST_CONTAINER} input`) || null;
+// CREATE SITE LIST WITH ITEMS FROM SYNC STORAGE
+await createSiteList().then(() => {
+  console.log("SITE LIST CREATED");
+});
+
+const checkBoxes = getAll(`${SELECTORS.SITE_LIST_CONTAINER} input`) || null;
 
 // EVENT LISTENERS
 
-addSiteButtonEl.addEventListener("click", () => {
+addSiteButtonEl.addEventListener("click", async (e) => {
   let value = addSiteInputEl.value ?? "";
   value = value.trim();
   value = value.replace(/\s+/g, "");
 
-  if (!isValidInput(value)) {
+  const isValidInput = await validateInput(value);
+
+  console.log("INPUT :", isValidInput);
+
+  if (!isValidInput) {
     // TODO: Add error message
     return;
   }
+
   try {
-    const url = new URL(value).origin;
-    if (!isValidInput(url)) return;
-    addNewSite(url);
-    createSiteList();
+    const url = new URL(value).origin || null;
+    if (!url) throw new Error("Invalid URL");
+
+    await addNewSite(url);
+    await createSiteList();
   } catch (error) {
     console.error(error);
     alert("Invalid URL");
   }
 });
 
-inputs?.forEach((input) => {
-  input.addEventListener("change", (e) => {
-    const oldSites = JSON.parse(localStorage.getItem("sitesList")) || null;
-    if (!oldSites) return;
+checkBoxes?.forEach((input) => {
+  input.addEventListener("change", async (e) => {
+    const oldSitesList = await getSitesList();
+    if (!oldSitesList || oldSitesList.length <= 0) return;
 
-    const updatedSites = [...oldSites].map((site) =>
+    const updatedSites = oldSitesList.map((site) =>
       site.url === e.target.id ? { ...site, checked: e.target.checked } : site
     );
-    localStorage.setItem("sitesList", JSON.stringify(updatedSites));
+
+    await chrome.storage.sync.set({ sitesList: updatedSites });
+
+    await createSiteList();
   });
 });
 
-// FUNCTIONS
 
-function addNewSite(url) {
-  const oldSites = JSON.parse(localStorage.getItem("sitesList")) || [];
-  const newSite = { url, checked: true };
-  localStorage.setItem("sitesList", JSON.stringify([...oldSites, newSite]));
+
+
+// LOGIC FUNCTIONS
+async function addNewSite(url) {
+  const oldSitesList = await getSitesList();
+
+  const newSitesList = [...oldSitesList, { url, checked: true }];
+  chrome.storage.sync.set({ sitesList: newSitesList }, () => {
+    console.table("SAVING NEW SITE");
+  });
 }
 
-function isValidInput(url) {
-  const isValidUrl = url.startsWith("http://") || url.startsWith("https://");
-  const alreadyExistsInLocalStorage = JSON.parse(
-    localStorage.getItem("sitesList")
-  )?.some((site) => site.url === url);
-
-  return isValidUrl && !alreadyExistsInLocalStorage;
+async function removeSite(url) {
+  const oldSitesList = await getSitesList();
+  const newSitesList = oldSitesList.filter((site) => site.url !== url);
+  chrome.storage.sync.set({ sitesList: newSitesList }, () => {
+    console.table("REMOVING SITE");
+  });
 }
 
-function createSiteList() {
+async function createSiteList() {
   siteListContainerEl.innerHTML = "";
 
-  const sitesList = JSON.parse(localStorage.getItem("sitesList")) || null;
-  if (!sitesList) return;
+  let sitesList = await getSitesList();
+
+  if (!sitesList || sitesList.length <= 0) return;
 
   sitesList.forEach((site) => {
     const siteEl = document.createElement("div");
@@ -97,4 +114,23 @@ function createSiteList() {
 
     siteListContainerEl.appendChild(siteEl);
   });
+}
+
+// HELPERS FUNCTIONS
+async function validateInput(url) {
+  const isValidUrl = url.startsWith("http://") || url.startsWith("https://");
+  const oldSitesList = await getSitesList();
+  const doesNotExist = oldSitesList.every((site) => site.url !== url);
+
+  console.log("URL :", url);
+  console.log("isValidUrl :", isValidUrl);
+  console.log("doesNotExist :", doesNotExist);
+
+  return isValidUrl && doesNotExist;
+}
+
+async function getSitesList() {
+  const items = await chrome.storage.sync.get([SITES_LIST]);
+
+  return items?.sitesList ?? [];
 }
