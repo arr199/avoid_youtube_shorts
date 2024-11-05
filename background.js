59c60b3ef8c624643ecf6c798a747006f4d97d4e
arr199 <abieledelso@gmail.com>
2024-11-05 19:18:58 +0000

    refactor

--- background.js
+++ background.js
@@ -1,12 +1,17 @@
 const CONSTANTS = {
-  AVOID_SHORTS: "avoid_shorts",
+  AVOID_WEBSITE: "avoid_website",
+  MUTE: "muteTab",
 };
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == "complete") {
-    let message = CONSTANTS.AVOID_SHORTS;
+    let message = CONSTANTS.AVOID_WEBSITE;
     chrome.tabs.sendMessage(tab.id, message);
   }
 });
 
-
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === CONSTANTS.MUTE) {
+    chrome.tabs.update(sender.tab.id, { muted: true });
+  }
+});
